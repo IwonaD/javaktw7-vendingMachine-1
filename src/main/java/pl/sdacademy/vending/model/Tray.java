9ae3189fd80428b94aba459da7c7a5dd4e5ecef7
IwonaD <Iwonadanel1>
2018-09-08 12:27:29 +0200

    after adding builder

--- src/main/java/pl/sdacademy/vending/model/Tray.java
+++ src/main/java/pl/sdacademy/vending/model/Tray.java
@@ -10,9 +10,15 @@ public class Tray {
     private Queue<Product> products;
 
 
-    public Tray(String symbol) {
-        this.symbol = symbol;
-        products = new LinkedList<>();
+    private Tray(Builder builder) {
+        this.symbol = builder.symbol;
+        this.price = builder.price;
+        this.products = builder.products;
+
+    }
+
+    public static Builder builder(String symbol){
+        return new Builder(symbol);
     }
 
     public String getSymbol() {
@@ -20,15 +26,43 @@ public class Tray {
 
     }
 
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
 
     public Long getPrice() {
         return price;
     }
 
-    public void setPrice(Long price) {
-        this.price = price;
+
+
+    public static class Builder {
+        private String symbol;
+        private Long price;
+        private Queue<Product> products;
+
+        private Builder(String symbol) {
+            if (symbol ==null){
+                throw new IllegalArgumentException("Tray symbol cannot be null");
+            }
+            this.symbol = symbol;
+            this.products = new LinkedList();
+
+
+        }
+
+        public Builder price (Long price) {
+            this.price = price;
+            return this;
+        }
+        public Builder product (Product product) {
+            this.products.add(product);
+            return this;
+        }
+
+        public Tray build() {
+            if (price ==null){
+                price = 990L;
+            }
+            return new Tray(this);
+        }
+
     }
 }
