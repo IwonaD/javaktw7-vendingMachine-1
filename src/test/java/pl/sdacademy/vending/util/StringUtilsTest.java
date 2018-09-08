c689e1e537d9f01ac7f65f7617d27b161f79143f
IwonaD <Iwonadanel1>
2018-09-08 14:49:28 +0200

    after adding StringUtils class and after tests

---
+++ src/test/java/pl/sdacademy/vending/util/StringUtilsTest.java
@@ -0,0 +1,68 @@
+package pl.sdacademy.vending.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringUtilsTest {
+
+    @Test
+    public void shouldNotModifyMatchingText() {
+        //given
+        String textToMatch = "Pawel";
+        Integer expectedTextLength = 5;
+
+        //when
+        String adjustedText = StringUtils.adjustText(textToMatch, expectedTextLength);
+
+        //then
+        assertEquals("Pawel", adjustedText);
+
+    }
+
+    @Test
+    public void shouldTrimTextThatIsTooLong() {
+        //given
+        String textToAdjust = "Ala ma kota";
+        Integer expectedTextLength = 8;
+
+        //when
+        String adjustedText = StringUtils.adjustText(textToAdjust,expectedTextLength);
+
+        //then
+        assertEquals("Ala ma k", adjustedText);
+    }
+
+    @Test
+    public void shouldCenterTextWhenSpacesAreEqual() {
+        //given
+        String textToAdjust = "Ala";
+        Integer expectedTextLength = 7;
+
+
+        //when
+        String adjustedText = StringUtils.adjustText(textToAdjust,expectedTextLength);
+
+
+        //then
+        assertEquals("  Ala  ", adjustedText);
+    }
+    @Test
+    public void shouldCenterTextWhenSpacesAreNotEqual() {
+        //given
+        String textToAdjust = "Ala";
+        Integer expectedTextLength = 8;
+
+
+        //when
+        String adjustedText = StringUtils.adjustText(textToAdjust,expectedTextLength);
+
+
+        //then
+        assertEquals("   Ala  ", adjustedText);
+    }
+
+
+
+
+}
